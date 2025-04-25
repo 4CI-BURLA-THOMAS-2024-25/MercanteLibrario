174744d8bf7d331e0a59df4309073b34d677021d
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2025-04-25 19:37:17 +0200

    implemento aggiunta venditore in DB

--- GestoreLibri/src/scriptGestoreVenditori.ts
+++ GestoreLibri/src/scriptGestoreVenditori.ts
@@ -1,3 +1,8 @@
+import { Venditore } from "./Venditore";
+
+//database
+let database: IDBDatabase;
+
 //prelevo reference del bottone per aggiungere venditori e vi associo la relativa funzione
 const bottoneAggiungiVenditore = document.getElementById("aggiungiVenditore") as HTMLButtonElement;
 bottoneAggiungiVenditore?.addEventListener("click", apriRegistrazioneVenditore);
@@ -12,6 +17,9 @@ bottoneRegistraVenditore?.addEventListener("click", registraVenditore);
 //prelevo reference del bottone per annullare l'aggiunta del venditore
 const bottoneChiudiPopup = document.getElementById("chiudiPopup");
 bottoneChiudiPopup?.addEventListener("click", chiudiRegistrazioneVenditore);
+
+//prelevo reference delle caselle di input
+const caselleInput = document.querySelectorAll(".testoInputPopup");
 
 // funzione per aprire il database
 function apriDatabase(): Promise<IDBDatabase>{
@@ -50,5 +58,41 @@ function chiudiRegistrazioneVenditore(): void{
 }
 
 function registraVenditore(): void{
-    
+    //array in cui salvo l'inpuit dell'utente e che uso per creare nuovo oggetto venditore
+    let datiVenditore: any[] = new Array(6);
+
+    //leggo tutte le caselle
+    for(let i = 0; i < caselleInput.length; i++){
+        datiVenditore[i] = (caselleInput[i] as HTMLInputElement).value;
+    }
+
+    //creo oggetto venditore
+    const venditore: Venditore = new Venditore(datiVenditore[0], datiVenditore[1], datiVenditore[2], datiVenditore[3], datiVenditore[4], datiVenditore[5]);
+
+    //apro transazione verso object store dei venditori, in scrittura
+    const transazione = database.transaction("Vneditori", "readwrite");
+    //salvo reference dell'object store
+    const tabellaVenditori = transazione.objectStore("Venditori");
+    //richiesta di aggiunta all'object store
+    const richiestaAggiuntaVenditore = tabellaVenditori.add(venditore);
+
+    //aggiunta andata a buon fine
+    richiestaAggiuntaVenditore.onsuccess = () => {
+
+    }
+
+    //errore, venditore già presente
+    richiestaAggiuntaVenditore.onerror = () => {
+        console.log("Libro già presente");
+    }
 }
+
+// al caricamento della pagina, apro database
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        database = await apriDatabase();
+        console.log("Database aperto:", database.name);
+    } catch (erroreDB) {
+        console.error("Errore apertura DB:", erroreDB);
+    }
+});
