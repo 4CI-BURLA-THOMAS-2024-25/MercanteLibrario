23c13234f8ca6c490e805c2c92df3bc49580c436
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2025-05-06 18:34:41 +0200

    termino gestione venditori

--- GestoreLibri/src/scriptGestoreVenditori.ts
+++ GestoreLibri/src/scriptGestoreVenditori.ts
@@ -79,8 +79,8 @@ function registraVenditore(): void{
     //aggiunta andata a buon fine
     richiestaAggiuntaVenditore.onsuccess = () => {
 
-        //aggiorno lista venditori
-        aggiornaListaVenditori();
+        //aggiorno lista venditori, rileggendo da DB
+        prelevaVenditori();
 
         //chiudo popup di inserimento
         chiudiRegistrazioneVenditore();
@@ -88,12 +88,8 @@ function registraVenditore(): void{
 
     //errore, venditore già presente
     richiestaAggiuntaVenditore.onerror = () => {
-        console.log("Libro già presente");
+        console.log("Venditore già presente");
     }
-}
-
-function aggiornaListaVenditori(): void{
-
 }
 
 //prelevo elenco completo vendirori da DB
@@ -102,17 +98,24 @@ async function prelevaVenditori(): Promise<void>{
     const transazione = database.transaction("Venditori", "readonly");
     //prelevo reference tabella libri del database
     const tabellaVenditori = transazione.objectStore("Venditori");
-    //array dei venditori
-    const venditori: Venditore[] = await new Promise<any[]>((resolve, reject) => {
-        //ottengo tutti venditori
-        const richiesta = tabellaVenditori.getAll();
-        //accetto richiesta e restituisco risultato
-        richiesta.onsuccess = () => resolve(richiesta.result);
-        //rifiuto richiesta e restituisco errore
-        richiesta.onerror = () => reject(richiesta.error);
-    });
 
-    mostraVenditori(venditori);
+    //gestisco eventuale errore di lettura da DB
+    try{
+        //array dei venditori
+        const venditori: Venditore[] = await new Promise<any[]>((resolve, reject) => {
+            //ottengo tutti venditori
+            const richiesta = tabellaVenditori.getAll();
+            //accetto richiesta e restituisco risultato
+            richiesta.onsuccess = () => resolve(richiesta.result);
+            //rifiuto richiesta e restituisco errore
+            richiesta.onerror = () => reject(richiesta.error);
+        });
+    
+        //se la lettura non genera errore, chiamo funzione per mostrare lista dei venditori
+        mostraVenditori(venditori);
+    }catch(errore){
+        window.alert("Errore durante il caricamento dei venditori:" + errore);
+    }
 }
 
 //mostro in tabella elenco che mi viene passato
@@ -126,19 +129,67 @@ async function mostraVenditori(venditori: Venditore[]): Promise<void>{
     for(let i = 0; i < venditori.length; i++){
         //creo riga con le info del venditore
         const riga: HTMLTableRowElement = document.createElement("tr");
-        riga.innerHTML = `<td>${venditori[i].codFiscale}</td><td>${venditori[i].nome}</td><td>${venditori[i].cognome}</td><td>${venditori[i].email}</td><td>${venditori[i].nTelefono}</td><td>${venditori[i].classe}</td><td>${venditori[i].soldiDaDare}</td>`;
 
-        //creo bottone per mostrare le copie associate a ciascun venditore
-        const bottone = document.createElement("button");
+        //creo cella del bottone di eliminazione
+        const cellaBottoneRimuoviVenditore  = document.createElement("td");
+        //creo bottone per "eliminare" il venditore
+        const bottoneRimuoviVenditore = document.createElement("button");
         //aggiungo testo al bottone
-        bottone.textContent = "Visualizza copie consegnate";
-        //associo ascoltatore e passo riga
-        bottone.addEventListener("click", () => mostraCopieVenditore(venditori[i].codFiscale));
-
+        bottoneRimuoviVenditore.textContent = "Rimuovi venditore";
+        //associo ascoltatore
+        bottoneRimuoviVenditore.addEventListener("click", () => rimuoviVenditore(venditori[i].codFiscale));
+        //inserisco bottone nella sua cella
+        cellaBottoneRimuoviVenditore.appendChild(bottoneRimuoviVenditore);
         //aggiungo cella alla riga
-        const cella = riga.insertCell();
+        riga.appendChild(cellaBottoneRimuoviVenditore);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaCF = document.createElement("td");
+        cellaCF.textContent = venditori[i].codFiscale;
+        riga.appendChild(cellaCF);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaNome = document.createElement("td");
+        cellaNome.textContent = venditori[i].nome;
+        riga.appendChild(cellaNome);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaCognome = document.createElement("td");
+        cellaCognome.textContent = venditori[i].cognome;
+        riga.appendChild(cellaCognome);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaEmail = document.createElement("td");
+        cellaEmail.textContent = venditori[i].email;
+        riga.appendChild(cellaEmail);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaTelefono = document.createElement("td");
+        cellaTelefono.textContent = String(venditori[i].nTelefono);
+        riga.appendChild(cellaTelefono);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaClasse = document.createElement("td");
+        cellaClasse.textContent = venditori[i].classe;
+        riga.appendChild(cellaClasse);
+
+        //creo cella codicefiscale e la aggiungo alla riga
+        const cellaSoldi = document.createElement("td");
+        cellaSoldi.textContent = String(venditori[i].soldiDaDare);
+        riga.appendChild(cellaSoldi);
+
+        //creo cella con bottone per vedere copie
+        const cellaBottoneVediCopie = document.createElement("td");
+        //creo bottone per mostrare le copie associate a ciascun venditore
+        const bottoneCopieVenditore = document.createElement("button");
+        //aggiungo testo al bottone
+        bottoneCopieVenditore.textContent = "Visualizza copie consegnate";
+        //associo ascoltatore e passo riga
+        bottoneCopieVenditore.addEventListener("click", () => mostraCopieVenditore(venditori[i].codFiscale));
+        //aggiungo cella alla riga
+        cellaBottoneVediCopie.appendChild(bottoneCopieVenditore);
         //inserisco bottone nella cella
-        cella.appendChild(bottone);
+        riga.appendChild(cellaBottoneVediCopie);
 
         //inserisco riga nel corpo della tabella
         corpoVenditori.appendChild(riga);
@@ -155,11 +206,56 @@ function mostraCopieVenditore(codFiscale: string): void{
     window.open(`gestoreCopieVenditore.html?codFiscale=${codFiscale}`, "_blank", `menubar=no", height=${altezza}, width=${larghezza}, top=0, left=0`);
 }
 
+async function rimuoviVenditore(codiceFiscale: string): Promise<void>{
+    //chiedo conferma per la cancellazione
+    const confermaCancellazioneVenditore = window.confirm("Sei sicuro di voler cancellare il venditore?");
+
+    //conferma della cancellazione
+    if(confermaCancellazioneVenditore){
+        //gestisco eventuali errori
+        try{
+            //apro transazione verso tabella dei venditori, in modifica
+            const transazione = database.transaction("Venditori", "readwrite");
+            //prelevo reference della tabella dei venditori
+            const tabellaVenditori = transazione.objectStore("Venditori");
+        
+            //richiedo la cancellazione, passando la chiave primaria
+            const richiesta = tabellaVenditori.delete(codiceFiscale);
+    
+            //gestisco transazione DB
+            await new Promise<void>((resolve, reject) => {
+        
+                richiesta.onsuccess = () => {
+                    window.alert("Venditore eliminato con successo");
+                    resolve();
+                };
+        
+                richiesta.onerror = () => {
+                    console.error("Errore nella cancellazione:", richiesta.error);
+                    reject(richiesta.error);
+                };
+            });
+    
+            //aggiorno tabella rileggendo da DB
+            prelevaVenditori();
+    
+        }catch(errore){
+            window.alert("Errore nella cancellazione:" + errore);
+        }
+
+    }else{
+        window.alert("Eliminazione annullata");
+    }
+
+}
+
 // al caricamento della pagina, apro database
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         database = await apriDatabase();
         console.log("Database aperto:", database.name);
+
+        await prelevaVenditori();
     } catch (erroreDB) {
         console.error("Errore apertura DB:", erroreDB);
     }
