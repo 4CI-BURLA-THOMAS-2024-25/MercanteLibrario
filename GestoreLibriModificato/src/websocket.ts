63cb583e94e61692e0831bb5d4c42cce07d1a686
3CI_BURLA_THOMAS_2023_24 <20164@studenti.marconiverona.edu.it>
2025-06-12 15:21:30 +0200

    update lista libri

--- GestoreLibriModificato/src/websocket.ts
+++ GestoreLibriModificato/src/websocket.ts
@@ -1,3 +1,3 @@
 //connessione tra host
- export const ws = new WebSocket('wss://appsdev.marconivr.it/libraio/serverNode')
+ export const ws = new WebSocket('wss://websocket.marconivr.it')
 //export const ws = new WebSocket('wss://appsdev.marconivr.it:8081')
